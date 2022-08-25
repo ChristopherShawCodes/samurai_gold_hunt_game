41810ca25e8affa1b22473e032348bab60438729
Christopher Shaw <FramedGamesCreations@gmail.com>
2022-08-25 17:51:17 -0500

    Update With Styling

--- server.py
+++ server.py
@@ -44,6 +44,11 @@ def process():
     return redirect("/")
 
 
+@app.route('/reset')
+def reset():
+    session.clear()
+    return redirect('/')
+
 
 if __name__=="__main__":
     app.run(debug=True)
